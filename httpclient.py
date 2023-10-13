7ba8f3bca04bfdc532a8239cc70b450637cffe92
zijing8 <zijinglu9@gmail.com>
2023-10-13 13:13:26 -0600

    done

--- httpclient.py
+++ httpclient.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # coding: utf-8
 # Copyright 2016 Abram Hindle, https://github.com/tywtyw2002, and https://github.com/treedust
-# 
+# Copyright 2023 Zijing Lu
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
